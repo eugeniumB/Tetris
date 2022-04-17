68b2356276af5a087f9a1d41dd429d61a792407f
eugeniumB <iffgenius@mail.ru>
2022-04-17 23:34:47 +0300

    Create Square.cs
    Реализован класс Square

---
+++ Tetris/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class Square
+    {
+        Point[] points = new Point[4];
+
+        public Square(Point p)
+        {
+            points[0] = new Point(p.x, p.y);
+            points[1] = new Point(p.x + 1, p.y);
+            points[2] = new Point(p.x, p.y + 1);
+            points[3] = new Point(p.x + 1, p.y + 1);
+        }
+        public void DrawSquare()
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].Draw();
+            }
+        }
+    }
+}
