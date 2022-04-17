3ac633f7980d8753d4571a81b95ded1de33af8b3
eugeniumB <iffgenius@mail.ru>
2022-04-17 23:34:08 +0300

    Create Point.cs
    Реализован класс Point

---
+++ Tetris/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class Point
+    {
+        public int x, y;
+        char c;
+
+        public Point(int x, int y, char symb = '*')
+        {
+            this.x = x;
+            this.y = y;
+            c = symb;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+
+    }
+}
