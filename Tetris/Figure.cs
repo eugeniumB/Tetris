33895802c3276506827246f901c68ebedd459eb2
eugeniumB <iffgenius@mail.ru>
2022-04-19 23:55:34 +0300

    Create Figure.cs

---
+++ Tetris/Figure.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    abstract class Figure
+    {
+        protected Point[] points = new Point[4];
+
+        public void Draw()
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].DrawPoint();
+            }
+        }
+
+        public void Move(Direction dir)
+        {
+            foreach (Point point in points)
+            {
+                point.Move(dir);
+            }
+        }
+
+        public void Hide()
+        {
+            foreach (Point item in points)
+            {
+                item.Hide();
+            }
+        }
+
+        public abstract void Rotate();
+     
+    }
+}
