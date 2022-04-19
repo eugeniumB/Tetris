10dac3b4cf972408aae1e7e2e968592a9aa24ad6
eugeniumB <iffgenius@mail.ru>
2022-04-19 23:55:38 +0300

    Update Program.cs

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -7,15 +7,19 @@
             Console.SetWindowSize(40, 30);
             Console.SetBufferSize(40, 30);
 
-            Point p1 = new Point(2, 3);
-            Point p2 = new Point(4, 5);
-            Point p3 = new Point(6, 7);
+            Point p1 = new(2, 3);
+            Point p2 = new(4, 5);
+            Point p3 = new(6, 7);
 
-            Square square = new Square(p1);
-            square.DrawSquare();
-            Stick stick = new Stick(p3);
-            stick.DrawStick();
+            Figure[] figures = new Figure[2];
+            figures[0] = new Square(p1);
+            figures[1] = new Stick(p3);
 
+            figures[1].Draw();
+            Thread.Sleep(1000);
+            figures[1].Hide();
+            figures[1].Rotate();
+            figures[1].Draw();
 
             Console.ReadLine();
         }
