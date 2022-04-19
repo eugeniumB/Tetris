29c2c42d6cca940b7a4971bb8cec3c20c69f5ff2
eugeniumB <iffgenius@mail.ru>
2022-04-19 23:55:37 +0300

    Update Point.cs

--- Tetris/Point.cs
+++ Tetris/Point.cs
@@ -9,7 +9,7 @@ namespace Tetris
     internal class Point
     {
         public int x, y;
-        char c;
+        private char c;
 
         public Point(int x, int y, char symb = '*')
         {
@@ -17,11 +17,34 @@ namespace Tetris
             this.y = y;
             c = symb;
         }
-        public void Draw()
+        public void DrawPoint()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(c);
         }
 
+        internal void Move(Direction dir)
+        {
+
+
+            switch (dir)
+            {
+                case Direction.left:
+                    x -= 1;
+                    break;
+                case Direction.right:
+                    x += 1;
+                    break;
+                case Direction.down:
+                    y += 1;
+                    break;
+            }
+        }
+
+        internal void Hide()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(" ");
+        }
     }
 }
