7bad53e4894dc58f577d6741271d311317cd7d2b
eugeniumB <iffgenius@mail.ru>
2022-04-19 23:55:53 +0300

    Update Stick.cs

--- Tetris/Stick.cs
+++ Tetris/Stick.cs
@@ -6,24 +6,40 @@ using System.Threading.Tasks;
 
 namespace Tetris
 {
-    internal class Stick
+    internal class Stick : Figure
     {
-        Point[] points = new Point[4];
-
         public Stick(Point p)
         {
             points[0] = new Point(p.x, p.y);
-            points[1] = new Point(p.x + 1, p.y);
-            points[2] = new Point(p.x + 2, p.y);
-            points[3] = new Point(p.x + 3, p.y);
+            points[1] = new Point(p.x, p.y + 1);
+            points[2] = new Point(p.x, p.y + 2);
+            points[3] = new Point(p.x, p.y + 3);
         }
-        public void DrawStick()
+
+        public override void Rotate()
+        {
+            if (points[0].x == points[1].x)
+                SetHorisontal();
+            else
+                SetVertical();
+        }
+
+        private void SetVertical()
         {
             for (int i = 0; i < points.Length; i++)
             {
-                points[i].Draw();
+                points[i].x = points[0].x;
+                points[i].y = points[0].y + i;
             }
         }
 
+        private void SetHorisontal()
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].y = points[0].y;
+                points[i].x = points[0].x + i; 
+            }
+        }
     }
 }
