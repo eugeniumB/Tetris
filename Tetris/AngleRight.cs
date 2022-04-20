683a18cb31062fccb424abf728380daf1328e607
eugeniumB <iffgenius@mail.ru>
2022-04-20 23:28:10 +0300

    Create AngleRight.cs

---
+++ Tetris/AngleRight.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class AngleRight : Figure
+    {
+        public AngleRight(Point p)
+        {
+            points[0] = new Point(p.x, p.y);
+            points[1] = new Point(p.x + 1, p.y);
+            points[2] = new Point(p.x, p.y - 1);
+            points[3] = new Point(p.x, p.y - 2);
+        }
+
+        public override void Rotate()
+        {
+            if (points[3].y + 2 == points[0].y)
+                SetRight();
+            else
+                if (points[3].y - 2 == points[0].y)
+                SetLeft();
+            else
+                if (points[3].x + 2 == points[0].x)
+                SetDefalt();
+            else
+                SetConversaly();
+        }
+
+        private void SetConversaly()
+        {
+            points[1].x -= 1;
+            points[1].y -= 1;
+            points[2].x -= 1;
+            points[2].y += 1;
+            points[3].x -= 2;
+            points[3].y += 2;
+        }
+
+        private void SetDefalt()
+        {
+            points[1].x += 1;
+            points[1].y += 1;
+            points[2].x += 1;
+            points[2].y -= 1;
+            points[3].x += 2;
+            points[3].y -= 2;
+        }
+
+        private void SetLeft()
+        {
+            points[1].x += 1;
+            points[1].y -= 1;
+            points[2].x -= 1;
+            points[2].y -= 1;
+            points[3].x -= 2;
+            points[3].y -= 2;
+        }
+
+        private void SetRight()
+        {
+            points[1].x -= 1;
+            points[1].y += 1;
+            points[2].x += 1;
+            points[2].y += 1;
+            points[3].x += 2;
+            points[3].y += 2;
+        }
+
+    }
+}
