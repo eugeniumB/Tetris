6dc5d96fb74a4ec719bdd7b568ae6fc6ef432d5a
eugeniumB <iffgenius@mail.ru>
2022-04-20 23:28:15 +0300

    Create RandFig.cs

---
+++ Tetris/RandFig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal class RandFig
+    {
+        private Point _p;
+        private Random _rand = new Random();
+
+        public RandFig(Point p)
+        {
+            _p = p;
+        }
+
+        internal Figure GetNewFigure()
+        {
+            if (_rand.Next(0, 5) == 0)
+                return new Square(_p);
+            else
+                if (_rand.Next(0, 5) == 1)
+                return new Stick(_p);
+            else
+                if (_rand.Next(0, 5) == 2)
+                return new Tripod(_p);
+            else
+                if (_rand.Next(0, 5) == 3)
+                return new AngleLeft(_p);
+            else 
+                return new AngleRight(_p);
+        }
+    }
+}
