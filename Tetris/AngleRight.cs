332af7c2c794653e87ec48f2eef2abe0dd5e0659
eugeniumB <iffgenius@mail.ru>
2022-04-21 13:03:55 +0300

    Update AngleRight.cs

--- Tetris/AngleRight.cs
+++ Tetris/AngleRight.cs
@@ -8,9 +8,8 @@ namespace Tetris
 {
     internal class AngleRight : Figure
     {
-        public AngleRight(Point p)
+        public AngleRight(Point p) : base(p)
         {
-            points[0] = new Point(p.x, p.y);
             points[1] = new Point(p.x + 1, p.y);
             points[2] = new Point(p.x, p.y - 1);
             points[3] = new Point(p.x, p.y - 2);
