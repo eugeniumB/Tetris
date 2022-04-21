0190203eae8d3bd579db2908f30a2b720ebb0113
eugeniumB <iffgenius@mail.ru>
2022-04-21 16:32:43 +0300

    Update Program.cs

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -10,33 +10,89 @@
             Point p = new(20, 4);
 
             RandFig generator = new RandFig(p);
-            Figure s = null;
+            Figure s = generator.GetNewFigure();
 
             while (true)
             {
-                FigureFall(s, generator);
-            }
-
-            static void FigureFall(Figure fig, RandFig generator)
-            {
-                fig = generator.GetNewFigure();
-                fig.Draw();
-
+                //FigureFall(out s, generator);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    HandleKey(s, key);
+                }
                 for (int i = 0; i < Console.BufferHeight; i++)
                 {
-                    if (fig.PointArray[0].y == Console.BufferHeight - 1 ||
-                        fig.PointArray[1].y == Console.BufferHeight - 1 ||
-                        fig.PointArray[2].y == Console.BufferHeight - 1 ||
-                        fig.PointArray[3].y == Console.BufferHeight - 1)
+                    if (s.PointArray[0].y == Console.BufferHeight - 1 ||
+                        s.PointArray[1].y == Console.BufferHeight - 1 ||
+                        s.PointArray[2].y == Console.BufferHeight - 1 ||
+                        s.PointArray[3].y == Console.BufferHeight - 1)
                         break;
-                    fig.Hide();
-                    fig.Move(Direction.down);
-                    fig.Draw();
-                    Thread.Sleep(200);
+                    //s.Hide();
+                    //s.Move(Direction.down);
+                    //s.Draw();
+                    //Thread.Sleep(200);
                 }
+
             }
 
+            //static void FigureFall(out Figure fig, RandFig generator)
+            //{
+            //    fig = generator.GetNewFigure();
+            //    fig.Draw();
+
+            //    for (int i = 0; i < Console.BufferHeight; i++)
+            //    {
+            //        if (fig.PointArray[0].y == Console.BufferHeight - 1 ||
+            //            fig.PointArray[1].y == Console.BufferHeight - 1 ||
+            //            fig.PointArray[2].y == Console.BufferHeight - 1 ||
+            //            fig.PointArray[3].y == Console.BufferHeight - 1)
+            //            break;
+            //        fig.Hide();
+            //        fig.Move(Direction.down);
+            //        fig.Draw();
+            //        Thread.Sleep(200);
+            //    }
+            //}
+
             Console.ReadLine();
+        }
+
+        private static void HandleKey(Figure s, ConsoleKeyInfo key)
+        {
+            switch (key.Key)
+            { 
+                case ConsoleKey.Spacebar:
+                    s.Rotate();
+                    break;
+                case ConsoleKey.LeftArrow:
+                    s.Move(Direction.left);
+                    break;
+                case ConsoleKey.RightArrow:
+                    s.Move(Direction.right);
+                    break;
+                case ConsoleKey.DownArrow:
+                    s.Move(Direction.down);
+                    break;
+                case ConsoleKey.A:
+                    s.Move(Direction.left);
+                    break;
+                case ConsoleKey.D:
+                    s.Move(Direction.right);
+                    break;
+                case ConsoleKey.S:
+                    s.Move(Direction.down);
+                    break;
+                case ConsoleKey.NumPad2:
+                    s.Move(Direction.down);
+                    break;
+                case ConsoleKey.NumPad4:
+                    s.Move(Direction.left);
+                    break;
+                case ConsoleKey.NumPad6:
+                    s.Move(Direction.right);
+                    break;
+            }
+
         }
     }
     
