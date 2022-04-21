32eb9274784b196177247bce66606f7817f3a877
eugeniumB <iffgenius@mail.ru>
2022-04-21 13:03:53 +0300

    Update AngleLeft.cs

--- Tetris/AngleLeft.cs
+++ Tetris/AngleLeft.cs
@@ -8,9 +8,8 @@ namespace Tetris
 {
     internal class AngleLeft : Figure
     {
-        public AngleLeft(Point p)
+        public AngleLeft(Point p) : base(p)
         {
-            points[0] = new Point(p.x, p.y);
             points[1] = new Point(p.x - 1, p.y);
             points[2] = new Point(p.x, p.y - 1);
             points[3] = new Point(p.x, p.y - 2);
