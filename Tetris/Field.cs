73bce8fbf3a33ded820c118367d5306ae1d7fd02
eugeniumB <iffgenius@mail.ru>
2022-04-24 21:35:35 +0300

    Create Field.cs

---
+++ Tetris/Field.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal static class Field
+    {
+        private static int _windowHeight = 30;
+        private static int _windowWidth = 40;
+
+        public static int Width
+        {
+            get 
+            { 
+                return _windowWidth;
+            }
+            private set 
+            {
+                _windowWidth = value;
+                Console.SetWindowSize(_windowWidth, Field.Height);
+                Console.SetBufferSize(_windowWidth, Field.Height);
+            }
+        }
+
+        public static int Height
+        {
+            get
+            {
+                return _windowHeight; 
+            }
+            private set
+            { 
+                _windowHeight = value; 
+                Console.SetWindowSize(_windowHeight, Field.Width);
+                Console.SetBufferSize(_windowHeight, Field.Width);
+            }
+        }
+
+        private static bool[][] _heap;
+
+        static Field()
+        {
+            _heap = new bool[Height][];
+            for (int i = 0; i < Height; i++)
+            {
+                _heap[i] = new bool[Width];
+            }
+        }
+
+        public static bool CheckStike(Point p)
+        {
+            return _heap[p.Y][p.X];
+        }
+
+        public static void AddFigure(Figure fig)
+        {
+            foreach (var p in fig.Points)
+            {
+                _heap[p.Y][p.X] = true;
+            }
+        }
+
+    }
+}
