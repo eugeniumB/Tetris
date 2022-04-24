2e240ad9c5b7f7899272ee19518a0b03f803d1c7
eugeniumB <iffgenius@mail.ru>
2022-04-24 21:35:31 +0300

    Create CheckStrike.cs

---
+++ Tetris/CheckStrike.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal enum CheckStrike
+    {
+        BORDER_STRIKE,
+        DOWN_STRIKE,
+        HEAP_STRIKE
+    }
+}
