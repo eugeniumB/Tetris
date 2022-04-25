1e7879be843dd2570a3848642993c9304f30d155
eugeniumB <iffgenius@mail.ru>
2022-04-26 00:12:35 +0300

    Create Strike.cs

---
+++ Tetris/Strike.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal enum Strike
+    {
+        NOTHING,
+        DOWN_STRIKE,
+        BORDER_STRIKE,
+        HEAP_STRIKE
+    }
+}
