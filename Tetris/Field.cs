c8bbe28e8d1d1dead47baa7fbad0051f7d281478
eugeniumB <iffgenius@mail.ru>
2022-04-27 16:41:10 +0300

    Update Field.cs

--- Tetris/Field.cs
+++ Tetris/Field.cs
@@ -41,20 +41,12 @@ namespace Tetris
 
         private static bool[][] _heap;
 
-        public static Point[][] Heap { get; set; }
-
         static Field()
         {
             _heap = new bool[Height][];
             for (int i = 0; i < Height; i++)
             {
                 _heap[i] = new bool[Width];
-            }
-
-            Heap = new Point[Height][];
-            for (int i = 0; i < Height; i++)
-            {
-                Heap[i] = new Point[Width];
             }
         }
 
@@ -69,13 +61,6 @@ namespace Tetris
             {
                 _heap[p.Y][p.X] = true;
             }
-        }
-
-        public static Point[][] NextHeap(Point[][] heap)
-        {
-            
-
-            return heap;
         }
 
     }
