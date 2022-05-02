560e185bb8330a47c2211055108d2d88658f9e00
eugeniumB <iffgenius@mail.ru>
2022-05-02 22:06:52 +0300

    Create IDrawer.cs

---
+++ TetrisGui/IDrawer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal interface IDrawer
+    {
+        void DrawPoint(int x, int y);
+        void HidePoint(int x, int y);
+        void GameOver();
+        void ShowScore();
+        void InitField();
+
+    }
+}
