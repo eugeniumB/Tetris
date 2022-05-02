e1ff34699e8277f728f7e7892991dcd6dc6cd06e
eugeniumB <iffgenius@mail.ru>
2022-05-02 22:06:31 +0300

    Create DrawerProvider.cs

---
+++ TetrisGui/DrawerProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    internal static class DrawerProvider
+    {
+        private static IDrawer _drawer = new GuiDrawer();
+
+        public static IDrawer Drawer
+        {
+            get { return _drawer; }
+        }
+    }
+}
